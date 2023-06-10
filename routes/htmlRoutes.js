a95467bd0e8b960c104af00c1e8a0ba303e79b6c
Enrique Sanchez <esanchez8k@github.com>
2023-06-10 18:53:25 -0400

    created the necessary HTML routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+// Route for the landing page
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Route for the notes page
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports = router;
